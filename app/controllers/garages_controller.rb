3d327ee59d766168bc04c6b6184ab10e952a0cee
Gerard Mota <gmotacastillo@gmail.com>
2022-11-29 13:41:33 +0100

    added views and controller

---
+++ app/controllers/garages_controller.rb
@@ -0,0 +1,22 @@
+class GaragesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+  def destroy
+  end
+end
