f0d742056cf227a7b563719e1c9bba3f390ac3df
mathis Krier <mathis.krier@hotmail.fr>
2022-11-30 11:43:07 +0100

    navbar functioning but sketchy

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,18 +1,47 @@
 import { Controller } from "@hotwired/stimulus"
+import { left } from "@popperjs/core"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["menu", "menutoggled", "page"]
+  initialize () {
+    this.isToggled = true
+  }
   connect() {
     console.log("connected")
     console.log(this.element)
+
   }
 
-  menu(event) {
-    //if element has class untoggled then add toggle class
-   console.log(event.currentTarget)
+  menu() {
+    // missing function: if menu collapsed then right div bigger (visversa)
+   console.log("you clicked the button")
+   console.log(this.menuTarget)
+   console.log(this.pageTarget)
+   if (this.isToggled) {
+
+    this.menuTarget.style.display = "none"
+    this.menutoggledTarget.style.display = "block"
+    //this.pageTarget.style= margin..
+    this.pageTarget.style.marginLeft = "100px";
+
+  } else {
+
+    this.menuTarget.style.display = "block"
+    this.menutoggledTarget.style.display = "none"
+    //this.pageTarget.style= margin..
+    this.pageTarget.style.marginLeft = "250px";
+
+   }
+  this.isToggled = !this.isToggled
   }
+
   //if element has toggle class then add untoggled class
-
+  //show () {
+  //  this.menu.style.display = "block"
+  //}
+  //hide () {
+  //  this.menu.style.display = "none"
+  //}
 
 }
