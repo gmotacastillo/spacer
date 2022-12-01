8b2b425f781259ae5b3a34a45cb13db37ddc8f72
Gerard Mota <gmotacastillo@gmail.com>
2022-12-01 18:39:47 +0100

    new view invoices

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,17 +4,20 @@
 
 import { application } from "./application"
 
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import InvoiceController from "./invoice_controller"
+application.register("invoice", InvoiceController)
+
+import NewinvoiceController from "./newinvoice_controller"
+application.register("newinvoice", NewinvoiceController)
 
 import NewlotController from "./newlot_controller"
 application.register("newlot", NewlotController)
 
 import ToggleController from "./toggle_controller"
 application.register("toggle", ToggleController)
-
-import FlatpickrController from "./flatpickr_controller"
-application.register("flatpickr", FlatpickrController)
-
-import InvoiceController from "./invoice_controller"
-application.register("invoice", InvoiceController)
