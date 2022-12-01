dc788794a84a75d3f0fa016e47c8bb5c3fb60db5
Robert Muller <mullerrobi93@gmail.com>
2022-12-01 10:37:05 +0100

    design changes on garage show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import CardflipController from "./cardflip_controller"
+application.register("cardflip", CardflipController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
