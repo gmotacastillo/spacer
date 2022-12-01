3a5e93717f6ae7b29befe3dfccbac115cfb9dda3
Robert Muller <mullerrobi93@gmail.com>
2022-12-01 14:40:47 +0100

    added third question about price

--- app/javascript/controllers/newlot_controller.js
+++ app/javascript/controllers/newlot_controller.js
@@ -2,16 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="newlot"
 export default class extends Controller {
-  static targets = ["questionOne", "questionTwo"]
+  static targets = ["questionOne", "questionTwo", "questionThree"]
 
   connect() {
     this.questionOneTarget.hidden = false;
     this.questionTwoTarget.hidden = true
+    this.questionThreeTarget.hidden = true
   }
 
   revealQuestionTwo() {
     this.questionOneTarget.hidden = true;
     this.questionTwoTarget.hidden = false;
+    this.questionThreeTarget.hidden = true;
   }
 
+  revealQuestionThree(){
+    this.questionOneTarget.hidden = true;
+    this.questionTwoTarget.hidden = true;
+    this.questionThreeTarget.hidden = false;
+  }
 }
