51f4548454bc2f82f9b806b29cc7cfa9a1a9c47f
Robert Muller <mullerrobi93@gmail.com>
2022-12-02 10:57:28 +0100

    pushing to heroku

--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -2,13 +2,11 @@ module GridHelper
   def set_columns(parking_spaces)
     case parking_spaces
     when 0..4
-     "1fr 1fr 1fr 1fr"
+      "1fr 1fr 1fr 1fr"
     when 4..10
-      "1fr 1fr"
-    when 10..19
-      "1fr 1fr 1fr"
-    when 20..30
-      "1fr 1fr 1fr 1fr 1fr"
+      "1fr 1fr 1fr 1fr"
+    when 10..30
+      "1fr 1fr 1fr 1fr 1fr 1fr"
     else
       "1fr 1fr 1fr 1fr 1fr 1fr 1fr"
     end
