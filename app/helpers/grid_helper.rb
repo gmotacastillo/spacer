85f07c666d55d839c9c15853a1516ba34ee812ee
Robert Muller <mullerrobi93@gmail.com>
2022-12-02 13:32:51 +0100

    calendar added and js

--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -1,9 +1,7 @@
 module GridHelper
   def set_columns(parking_spaces)
     case parking_spaces
-    when 0..4
-      "1fr 1fr 1fr 1fr"
-    when 4..10
+    when 0..10
       "1fr 1fr 1fr 1fr"
     when 10..30
       "1fr 1fr 1fr 1fr 1fr 1fr"
